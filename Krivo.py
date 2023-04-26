eb5a4d72240c0637ff4e3e95e8dc030bc9e0a87c
opilane <opilane@HP-CZC2349HV2>
2023-04-26 12:25:29 +0300

    Add project files.

---
+++ Krivo.py
@@ -0,0 +1,39 @@
+﻿import pygame, random
+pygame.init()
+
+#värvid
+red = [255,0,0]
+green = [0,255,0]
+blue = [0,0,255]
+pink = [255,153,255]
+lGreen = [153,255,153]
+lBlue = [153,204,255]
+
+#ekraani seaded
+screenX = 640
+screenY = 400
+screen=pygame.display.set_mode([screenX,screenY])
+pygame.display.set_caption("Hiir")
+screen.fill(lBlue)
+clock = pygame.time.Clock()
+
+gameover = False
+while not gameover:
+    clock.tick(10)
+    #mängu sulgemine ristist
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            gameover = True
+
+    click=pygame.mouse.get_pos()
+    r=random.randint(0, 255)
+    g=random.randint(0, 255)
+    b=random.randint(0, 255)
+    varv = [r,g,b]
+    ruuduke=pygame.Rect(click[0],click[1], 100, 100)
+    pygame.draw.rect(screen, varv, ruuduke)
+
+    pygame.display.flip()
+    screen.fill(lGreen)
+
+    pygame.quit()
